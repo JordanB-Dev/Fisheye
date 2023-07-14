a6ecd0c418955d2bf800289d5364fa853f896239
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-07-14 10:06:16 +0200

    form contact + css

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -36,66 +36,95 @@ const errorDisplay = (tag, message, valid) => {
   }
 };
 
+const addError = (border) => {
+  border.setAttribute("data-error", "true");
+  border.setAttribute("data-succes", "false");
+};
+
+const addSucces = (border) => {
+  border.setAttribute("data-error", "false");
+  border.setAttribute("data-succes", "true");
+};
+
 const firstChecker = (value) => {
+  const border = document.getElementById("first");
+
   if (value.length < 2 || value.length > 20) {
     errorDisplay(
       "first",
       "Veuillez entrer 2 caractères ou plus pour le champ prénom."
     );
+    addError(border);
     firstName = null;
   } else if (!value.match(regexText)) {
     errorDisplay(
       "first",
       "Le prénom ne doit pas contenir de caractères spéciaux"
     );
+    addError(border);
     firstName = null;
   } else {
     errorDisplay("first", "", true);
+    addSucces(border);
     firstName = value;
   }
 };
 
 const lastChecker = (value) => {
+  const border = document.getElementById("last");
+
   if (value.length < 2 || value.length > 20) {
     errorDisplay(
       "last",
       "Veuillez entrer 2 caractères ou plus pour le champ nom."
     );
+    addError(border);
     lastName = null;
   } else if (!value.match(regexText)) {
     errorDisplay("last", "Le nom ne doit pas contenir de caractères spéciaux");
+    addError(border);
     lastName = null;
   } else {
     errorDisplay("last", "", true);
+    addSucces(border);
     lastName = value;
   }
 };
 
 const emailChecker = (value) => {
+  const border = document.getElementById("email");
+
   if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
     errorDisplay("email", " L'adresse électronique n'est pas valide");
+    addError(border);
     email = null;
   } else {
     errorDisplay("email", "", true);
+    addSucces(border);
     email = value;
   }
 };
 
 const messageChecker = (value) => {
+  const border = document.getElementById("message");
+
   if (value.length < 20 || value.length > 150) {
     errorDisplay(
       "message",
       "Veuillez entrer 20 caractères minimum ou 150 caractères maximum."
     );
+    addError(border);
     message = null;
   } else if (!value.match(regexText)) {
     errorDisplay(
       "message",
       "Le nessage ne doit pas contenir de caractères spéciaux"
     );
+    addError(border);
     message = null;
   } else {
     errorDisplay("message", "", true);
+    addSucces(border);
     message = value;
   }
 };
