4f3510c356d121baf1da8b297d4c2c0a6982663b
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-07-14 05:04:42 +0200

    add aria-label

--- scripts/factory/photographer.js
+++ scripts/factory/photographer.js
@@ -17,24 +17,29 @@ const photographerTemplate = (data) => {
 
     img.setAttribute("src", picture);
     img.title = name;
-    img.alt = `Photo de ${name}`;
+    img.alt = `Photo du photographe ${name}`;
+    img.setAttribute("aria-label", `Photo du photographe ${name}`);
     article.appendChild(img);
     link.appendChild(img);
 
     h2.textContent = name;
+    h2.setAttribute("aria-label", "Nom du photographe");
     article.appendChild(h2);
     link.appendChild(h2);
 
     location.textContent = `${city} , ${country}`;
     location.classList.add("photograph-location");
+    location.setAttribute("aria-label", "Adresse du photographe");
     article.appendChild(location);
 
     content.textContent = tagline;
     content.classList.add("photograph-tagline");
+    content.setAttribute("aria-label", "Slogan du photographe");
     article.appendChild(content);
 
     pricing.textContent = `${price}€/jour`;
     pricing.classList.add("photograph-price");
+    pricing.setAttribute("aria-label", "Le prix journalier du photographe");
     article.appendChild(pricing);
 
     return article;
