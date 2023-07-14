74a6744559ff3bf2698ddebc9307f7499c47d8ef
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-07-14 09:33:32 +0200

    js form contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,14 @@
 const modal = document.getElementById("contact_modal");
 const main = document.getElementById("main");
+const form = document.querySelector("form");
 
 const inputs = document.querySelectorAll(
   'input[type="text"], input[type="email"], textarea[id="message"]'
 );
+
+let firstName, lastName, email, message;
+
+const regexText = /^[a-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ\s-]{1,31}$/i;
 
 const displayModal = () => {
   modal.style.display = "block";
@@ -19,28 +24,116 @@ const closeModal = () => {
   modal.setAttribute("aria-modal", "false");
 };
 
-const firstChecker = () => {};
-const lastChecker = () => {};
-const emailChecker = () => {};
-const messageChecker = () => {};
+const errorDisplay = (tag, message, valid) => {
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    span.classList.add("error");
+    span.textContent = message;
+  } else {
+    span.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+const firstChecker = (value) => {
+  if (value.length < 2 || value.length > 20) {
+    errorDisplay(
+      "first",
+      "Veuillez entrer 2 caractères ou plus pour le champ prénom."
+    );
+    firstName = null;
+  } else if (!value.match(regexText)) {
+    errorDisplay(
+      "first",
+      "Le prénom ne doit pas contenir de caractères spéciaux"
+    );
+    firstName = null;
+  } else {
+    errorDisplay("first", "", true);
+    firstName = value;
+  }
+};
+
+const lastChecker = (value) => {
+  if (value.length < 2 || value.length > 20) {
+    errorDisplay(
+      "last",
+      "Veuillez entrer 2 caractères ou plus pour le champ nom."
+    );
+    lastName = null;
+  } else if (!value.match(regexText)) {
+    errorDisplay("last", "Le nom ne doit pas contenir de caractères spéciaux");
+    lastName = null;
+  } else {
+    errorDisplay("last", "", true);
+    lastName = value;
+  }
+};
+
+const emailChecker = (value) => {
+  if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    errorDisplay("email", " L'adresse électronique n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+};
+
+const messageChecker = (value) => {
+  if (value.length < 20 || value.length > 150) {
+    errorDisplay(
+      "message",
+      "Veuillez entrer 20 caractères minimum ou 150 caractères maximum."
+    );
+    message = null;
+  } else if (!value.match(regexText)) {
+    errorDisplay(
+      "message",
+      "Le nessage ne doit pas contenir de caractères spéciaux"
+    );
+    message = null;
+  } else {
+    errorDisplay("message", "", true);
+    message = value;
+  }
+};
 
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
     switch (e.target.id) {
       case "first":
-        firstChecker(console.log(e.target.value));
+        firstChecker(e.target.value);
         break;
       case "last":
-        lastChecker(console.log(e.target.value));
+        lastChecker(e.target.value);
         break;
       case "email":
-        emailChecker(console.log(e.target.value));
+        emailChecker(e.target.value);
         break;
       case "message":
-        messageChecker(console.log(e.target.value));
+        messageChecker(e.target.value);
         break;
       default:
         nul;
     }
   });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (firstName && lastName && email && message) {
+    const data = {
+      firstName,
+      lastName,
+      email,
+      message,
+    };
+    console.log(data);
+    closeModal();
+  } else {
+    alert("veuillez remplir correctement les champs");
+  }
 });
