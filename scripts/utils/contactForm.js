6ae407313bebfed035578f0b870afe1ba174db91
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-07-14 08:31:43 +0200

    add form contact, accessible, js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,26 @@
+const modal = document.getElementById("contact_modal");
+const main = document.getElementById("main");
+
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="email"], textarea[id="message"]'
+);
+
 const displayModal = () => {
-  const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
+  main.setAttribute("aria-hidden", "true");
+  modal.setAttribute("aria-hidden", "false");
+  modal.setAttribute("aria-modal", "true");
 };
 
 const closeModal = () => {
-  const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
+  main.setAttribute("aria-hidden", "false");
+  modal.setAttribute("aria-hidden", "true");
+  modal.setAttribute("aria-modal", "false");
 };
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    console.log(e.target.value);
+  });
+});
