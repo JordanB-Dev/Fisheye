d475c462583b6bb3ebc370ded48e5910fff1dd9b
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-07-19 18:50:31 +0200

    add media video

--- scripts/factory/mediaFactory.js
+++ scripts/factory/mediaFactory.js
@@ -1,13 +1,21 @@
 const mediaTemplate = (data) => {
-  const { id, photographerId, title, image, likes, date, price } = data
+  const params = new URLSearchParams(window.location.search)
+  const idPage = params.get('id')
+  const { id, photographerId, title, image, video, likes, date, price } = data
+  const picture = `assets/images/medias/${idPage}/${image}`
+  const mediaVideo = `assets/images/medias/${idPage}/${video}`
 
   const getMediaCardDOM = () => {
     const article = document.createElement('article')
     const img = document.createElement('img')
-    img.setAttribute('src', 'test')
+    const videoDOM = document.createElement('video')
+    img.setAttribute('src', picture)
+    videoDOM.setAttribute('controls', 'controls')
+    videoDOM.setAttribute('src', mediaVideo)
     const h2 = document.createElement('h2')
     h2.textContent = 'test'
     article.appendChild(img)
+    article.appendChild(videoDOM)
     article.appendChild(h2)
     console.log(data)
 
@@ -17,7 +25,8 @@ const mediaTemplate = (data) => {
     id,
     photographerId,
     title,
-    image,
+    picture,
+    video,
     likes,
     date,
     price,
