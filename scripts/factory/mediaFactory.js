fd1b71c31861523cec927289c9f20f203a7c47c5
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-07-20 09:43:39 +0200

    add display spam likes media

--- scripts/factory/mediaFactory.js
+++ scripts/factory/mediaFactory.js
@@ -5,16 +5,19 @@ const mediaTemplate = (data) => {
   console.log(data)
 
   const getMediaCardDOM = () => {
+    const article = document.createElement('article')
+    const h2 = document.createElement('h2')
+    const span = document.createElement('span')
+
     if (video) {
       const mediaVideo = `assets/images/medias/${idPage}/${video}`
 
-      const article = document.createElement('article')
-
       const videoDOM = document.createElement('video')
-      const source = document.createElement('source')
       videoDOM.setAttribute('controls', 'controls')
       videoDOM.setAttribute('aria-label', `Video ${title}`)
       videoDOM.setAttribute('type', 'video')
+
+      const source = document.createElement('source')
       source.setAttribute('src', mediaVideo)
       source.setAttribute('aria-label', `Source de la video ${title}`)
       source.setAttribute('type', 'video/mp4')
@@ -22,15 +25,9 @@ const mediaTemplate = (data) => {
       article.appendChild(source)
       videoDOM.appendChild(source)
 
-      const h2 = document.createElement('h2')
-      h2.textContent = title
       h2.setAttribute('aria-label', 'Titre de la video')
-      article.appendChild(h2)
-
-      return article
     } else {
       const picture = `assets/images/medias/${idPage}/${image}`
-      const article = document.createElement('article')
 
       const img = document.createElement('img')
       img.title = title
@@ -39,13 +36,16 @@ const mediaTemplate = (data) => {
       img.setAttribute('src', picture)
       article.appendChild(img)
 
-      const h2 = document.createElement('h2')
-      h2.textContent = title
       h2.setAttribute('aria-label', 'Titre de l`image')
-      article.appendChild(h2)
-
-      return article
     }
+
+    h2.textContent = title
+    article.appendChild(h2)
+
+    span.textContent = likes
+    article.appendChild(span)
+
+    return article
   }
   return {
     getMediaCardDOM,
